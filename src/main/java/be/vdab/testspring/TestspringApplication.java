11063c682ef4ccbb2b424d1beecea3e45c20f1d3
andre <andre.komdeur@gmail.com>
2020-04-03 12:27:22 +0200

    Initial commit

---
+++ src/main/java/be/vdab/testspring/TestspringApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.testspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestspringApplication.class, args);
+	}
+
+}
